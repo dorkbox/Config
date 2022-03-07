d67dd34019cc0d5de6ffd34a4716f3bcf04a812b
Robinson <robinson@dorkbox.com>
2022-03-07 08:10:20 +0100

    Initial split of configuration.

---
+++ build.gradle.kts
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2020 dorkbox, llc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.time.Instant
+
+///////////////////////////////
+//////    PUBLISH TO SONATYPE / MAVEN CENTRAL
+////// TESTING : (to local maven repo) <'publish and release' - 'publishToMavenLocal'>
+////// RELEASE : (to sonatype/maven central), <'publish and release' - 'publishToSonatypeAndRelease'>
+///////////////////////////////
+
+gradle.startParameter.showStacktrace = ShowStacktrace.ALWAYS   // always show the stacktrace!
+
+
+plugins {
+    id("com.dorkbox.GradleUtils") version "2.16"
+    id("com.dorkbox.Licensing") version "2.12"
+    id("com.dorkbox.VersionUpdate") version "2.4"
+    id("com.dorkbox.GradlePublish") version "1.12"
+
+    kotlin("jvm") version "1.6.10"
+}
+
+object Extras {
+    // set for the project
+    const val description = "Configuration properties that can be defined via the CLI, system properties, environment variables, or file."
+    const val group = "com.dorkbox"
+    const val version = "1.0"
+
+    // set as project.ext
+    const val name = "Config"
+    const val id = "Config" // this is the maven ID!
+    const val vendor = "Dorkbox LLC"
+    const val vendorUrl = "https://dorkbox.com"
+    const val url = "https://git.dorkbox.com/dorkbox/Config"
+
+    val buildDate = Instant.now().toString()
+}
+
+///////////////////////////////
+/////  assign 'Extras'
+///////////////////////////////
+GradleUtils.load("$projectDir/../../gradle.properties", Extras)
+GradleUtils.defaults()
+GradleUtils.compileConfiguration(JavaVersion.VERSION_1_8) {
+    // see: https://kotlinlang.org/docs/reference/using-gradle.html
+    freeCompilerArgs = listOf(
+        // enable the use of experimental methods
+        "-Xopt-in=kotlin.RequiresOptIn"
+    )
+}
+//GradleUtils.jpms(JavaVersion.VERSION_1_9) moshi doesn't support JPMS yet
+
+
+licensing {
+    license(License.APACHE_2) {
+        description(Extras.description)
+        author(Extras.vendor)
+        url(Extras.url)
+    }
+}
+
+tasks.jar.get().apply {
+    manifest {
+        // https://docs.oracle.com/javase/tutorial/deployment/jar/packageman.html
+        attributes["Name"] = Extras.name
+
+        attributes["Specification-Title"] = Extras.name
+        attributes["Specification-Version"] = Extras.version
+        attributes["Specification-Vendor"] = Extras.vendor
+
+        attributes["Implementation-Title"] = "${Extras.group}.${Extras.id}"
+        attributes["Implementation-Version"] = Extras.buildDate
+        attributes["Implementation-Vendor"] = Extras.vendor
+    }
+}
+
+dependencies {
+    api("com.dorkbox:Updates:1.1")
+    api("com.dorkbox:OS:1.0")
+
+    val moshiVer = "1.13.0"
+
+    // For JSON serialization
+    api("com.squareup.moshi:moshi:$moshiVer")
+    api("com.squareup.moshi:moshi-kotlin:$moshiVer")
+    api("com.squareup.okio:okio:3.0.0")
+
+    // https://github.com/MicroUtils/kotlin-logging
+    api("io.github.microutils:kotlin-logging:2.1.21")
+    api("org.slf4j:slf4j-api:1.8.0-beta4")
+}
+
+publishToSonatype {
+    groupId = Extras.group
+    artifactId = Extras.id
+    version = Extras.version
+
+    name = Extras.name
+    description = Extras.description
+    url = Extras.url
+
+    vendor = Extras.vendor
+    vendorUrl = Extras.vendorUrl
+
+    issueManagement {
+        url = "${Extras.url}/issues"
+        nickname = "Gitea Issues"
+    }
+
+    developer {
+        id = "dorkbox"
+        name = Extras.vendor
+        email = "email@dorkbox.com"
+    }
+}
---
+++ src/dorkbox/config/Config.kt
@@ -0,0 +1,412 @@
+package dorkbox.config
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.JsonClass
+import com.squareup.moshi.Moshi
+import dorkbox.os.OS
+import mu.KLogger
+import java.io.File
+import java.lang.reflect.Modifier
+import java.util.*
+import mu.KotlinLogging.logger
+import kotlin.reflect.KProperty
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.findAnnotations
+import kotlin.reflect.full.hasAnnotation
+import kotlin.reflect.full.isSubclassOf
+import kotlin.reflect.jvm.javaField
+import kotlin.reflect.jvm.jvmErasure
+import kotlin.system.exitProcess
+
+/**
+ * we want to support configuration properties, such that ANYTHING in the config file can ALSO be passed in on the command line or as an env/system property
+ *
+ * ```
+ * commandline > system property > environment variable > properties file
+ * ```
+ * Once a property has been defined, it cannot be overloaded again via a different method, as specified in the above hierarchy,
+ *
+ * During a save operation, overloaded values will be ignored unless they were manually changed to something different
+ */
+@Suppress("MemberVisibilityCanBePrivate", "unused")
+open class Config<T: Any>(
+    /**
+     * Enables setting environment variable names that might conflict with the standard set of names, for example,
+     * PATH...
+     *
+     * The system uses PATH, but if we want to use path for something different, we can via setting the prefix.
+     * For example, setting the prefix to "CONFIG__" means that for us to set the `path` property, we set it via
+     *
+     *```
+     *  CONFIG__path="/home/blah/whatever"
+     *```
+     *  And this way, **OUR** path does not conflict with the system path.
+     */
+    private val environmentVarPrefix: String = "",
+    moshiAdapter: Moshi.Builder.() -> JsonAdapter<T>) {
+
+
+    companion object {
+        private val regexEquals = "=".toRegex()
+        private val locale = Locale.getDefault()
+
+        /**
+         * Gets the version number.
+         */
+        const val version = "1.0"
+
+        init {
+            // Add this project to the updates system, which verifies this class + UUID + version information
+            dorkbox.updates.Updates.add(Config::class.java, "a2afbd7d98084a9eb6eb663570dbec77", version)
+        }
+    }
+
+    private val logger: KLogger = logger() {}
+
+    private val moshi = moshiAdapter(Moshi.Builder())
+
+    private lateinit var configFile: File
+
+    private lateinit var originalConfig: T
+    private lateinit var originalConfigMap: Map<String, ConfigProp>
+
+    private lateinit var configMap: Map<String, ConfigProp>
+    private lateinit var config: T
+
+    private val arguments = ArrayList<String>()
+
+
+    /** Contains a list of the changed properties (via overloading) */
+    private val trackedConfigProperties = mutableMapOf<String, Any>()
+    private val originalOverloadedProperties = mutableMapOf<String, Any>()
+
+    fun load(configFile: File, createDefaultObject: () -> T): T {
+        this.configFile = configFile
+        var localConfig: T? = null
+        if (configFile.canRead()) {
+            val argFileContents = configFile.readText(Charsets.UTF_8)
+            if (argFileContents.isNotEmpty()) {
+                try {
+                    // we want to make ALL NEW configs, each referencing a DIFFERENT object
+                    localConfig = moshi.fromJson(argFileContents)!!
+                }
+                catch (ignored: Exception) {
+                }
+            }
+        }
+
+        if (localConfig == null) {
+            localConfig = createDefaultObject()
+            save()
+        }
+
+        // now, we make a COPY of the original values from the file (so when saving, we know what the overloaded values are and not save them)
+        originalConfig = localConfig
+        // create the map that knows what members have what values
+        originalConfigMap = createConfigMap(originalConfig)
+
+
+        // let's also make a deep copy.
+        config = moshi.fromJson(moshi.toJson(originalConfig))!!
+        // create the map that knows what members have what values
+        configMap = createConfigMap(config)
+
+        return config
+    }
+
+    fun save() {
+        val possiblyOverloadedConfigMap = configMap
+
+        // now, when we SAVE, we want to make sure that we DO NOT save overloaded values!
+        // we ONLY save the original values + any values that have been modified.
+        val changedProperties = mutableListOf<String>()
+        trackedConfigProperties.entries.forEach { (key, _) ->
+            // check to see if our overloaded value has been changed...
+            val inMemoryProperty = possiblyOverloadedConfigMap[key]
+            val originalOverloadedProp = originalOverloadedProperties[key]
+
+            // if it has changed, then REMOVE IT from the list of things to ignore
+            if (inMemoryProperty!!.get() != originalOverloadedProp) {
+                changedProperties.add(key)
+            }
+        }
+        changedProperties.forEach {
+            trackedConfigProperties.remove(it)
+        }
+
+
+        // copy all the in-memory values to the "original" config, but SKIP the ones that are overloaded (and unchanged).
+        // this is a tad slow, but makes sure that our saves do not include properties that have been overloaded
+        if (trackedConfigProperties.isNotEmpty()) {
+            possiblyOverloadedConfigMap.entries.forEach { (key, value) ->
+                if (!trackedConfigProperties.containsKey(key)) {
+                    originalConfigMap[key]!!.set(value.get())
+                }
+            }
+        }
+
+        val configToString = moshi.toJson(originalConfig)
+        configFile.writeText(configToString, Charsets.UTF_8)
+    }
+
+    /**
+     * process the arguments, and if applicable perform a `get` or `set` operation
+     */
+    fun process(arguments: Array<String>, onSaveAction: () -> Unit): List<String> {
+        this.arguments.addAll(arguments)
+
+        // now we have to see if there are any OVERLOADED properties
+        manageOverloadProperties()
+        manageGetAndSet(onSaveAction)
+
+        return this.arguments
+    }
+
+
+    private fun createConfigMap(config: Any): Map<String, ConfigProp> {
+        // this creates an EASY-to-use map of all arguments we have
+        val argumentMap = mutableMapOf<String, ConfigProp>()
+
+        val kClass = config::class
+        require(kClass.hasAnnotation<JsonClass>()) { "Cannot parse configuraion if it is not annotated with @JsonClass"}
+
+        // get all the members of this class.
+        for (member in kClass.declaredMemberProperties) {
+            @Suppress("UNCHECKED_CAST")
+            assignFieldsToMap(argumentMap, member as KProperty<Any>, config, member.getter.call(config)!!, "", -1)
+        }
+
+        return argumentMap
+    }
+
+    // the class is treated as lowercase, but the VALUE of properties is treated as case-sensitive
+    @OptIn(ExperimentalStdlibApi::class)
+    private fun assignFieldsToMap(argMap: MutableMap<String, ConfigProp>, field: KProperty<Any>,
+                                  parentObj: Any, obj: Any,
+                                  prefix: String, index: Int = -1)
+
+    {
+        if (Modifier.isTransient(field.javaField!!.modifiers)) {
+            // ignore transient fields!
+            return
+        }
+
+        var jsonName = (field.findAnnotations<Json>().firstOrNull()?.name ?: field.name).lowercase(locale)
+        jsonName = when {
+            prefix.isEmpty() -> jsonName
+            else             ->"$prefix.$jsonName".lowercase(locale)
+        }
+
+        if (index > -1) {
+            jsonName += "[$index]"
+        }
+
+        val kClass = obj::class
+        if (kClass.hasAnnotation<JsonClass>()) {
+            // get all the members of this class.
+            for (member in kClass.declaredMemberProperties) {
+                @Suppress("UNCHECKED_CAST")
+                assignFieldsToMap(argMap, member as KProperty<Any>, obj, member.getter.call(obj)!!, jsonName, -1)
+            }
+        } else {
+            if (field.returnType.jvmErasure.isSubclassOf(List::class)) {
+                // iterate over the collection, but assign the index with the name.
+                @Suppress("UNCHECKED_CAST")
+                val collection = obj as List<Any>
+                collection.forEachIndexed { i, any ->
+                    assignFieldsToMap(argMap, field, obj, any, prefix, i)
+                }
+            } else {
+                // only add fields to map that are ones we care about
+                @Suppress("UNCHECKED_CAST")
+                argMap[jsonName] = ConfigProp(parentObj, field)
+            }
+        }
+    }
+
+    private fun logAndExit(message: String) {
+        println(message)
+        exitProcess(0)
+    }
+    private fun logAndExit(argProp: ConfigProp?) {
+        if (argProp != null) {
+            println(argProp.get().toString())
+        } else {
+            println()
+        }
+        exitProcess(0)
+    }
+
+    /**
+     * Allows the ability to `get` or `set` configuration properties. Will call System.exit() if a get/set was done
+     */
+    private fun manageGetAndSet(onSaveAction: () -> Unit) {
+        if ((arguments.isEmpty())) {
+            // nothing to do
+            return
+        }
+
+        val args = arguments.map { it.lowercase(locale) }
+        val getIndex = args.indexOf("get")
+        val setIndex = args.indexOf("set")
+
+        if (getIndex > -1) {
+            val propIndex = getIndex + 1
+            if (propIndex > arguments.size - 1) {
+                logAndExit("Must specify property to get. For Example: 'get server.ip'")
+            }
+
+            val propToQuery = arguments[propIndex]
+            val prop = configMap[propToQuery]
+            logAndExit(prop)
+        } else if (setIndex > -1) {
+            val propIndex = setIndex + 1
+            if (propIndex > arguments.size - 1) {
+                logAndExit("Must specify property to set. For Example: 'set server.ip 127.0.0.1'")
+            }
+
+            val valueIndex = setIndex + 2
+            if (valueIndex > arguments.size - 1) {
+                logAndExit("Must specify property value to set. For Example: 'set server.ip 127.0.0.1'")
+            }
+
+            val propToSet = arguments[propIndex]
+            val valueToSet = arguments[valueIndex]
+
+            val prop = configMap[propToSet]
+            if (prop != null) {
+                // we output the OLD value, in case we want it from the CLI
+                val oldValue = prop.set(valueToSet)
+
+                // we ALWAYS want to re-save this file back
+                onSaveAction()
+
+                logAndExit(oldValue.toString())
+            } else {
+                // prop wasn't found
+                logAndExit("")
+            }
+        }
+
+        return
+    }
+
+    /**
+     * Overloaded properties can be of the form
+     * For strings (and all other supported types)
+     *    config.z="flag" (for a string)
+     *    config.x="true" (for a boolean)
+     *
+     * Additional support for booleans
+     *    config.x (absent any value is a 'true')
+     */
+    @Suppress("UNCHECKED_CAST", "DuplicatedCode")
+    private fun manageOverloadProperties() {
+        configMap.forEach { (arg, prop) ->
+            val returnType = prop.member.returnType.jvmErasure
+
+            if (prop.isSupported()) {
+                ////
+                // CLI CHECK IF PROPERTY EXIST  (explicit check for arg=value)
+                // if arg is found, no more processing happens
+                ////
+                var foundArg = arguments.firstOrNull { it.startsWith("$arg=") }
+                if (foundArg != null) {
+                    // we know that split[0] == 'arg=' because we already checked for that
+                    val split = foundArg.split(regexEquals)[1].trim()
+
+                    // save this, so we can figure out what the values are on save (and not save overloaded properties, that are unchanged)
+                    val get = prop.get()
+                    if (get != split) {
+                        // only track it if it's different
+                        trackedConfigProperties[arg] = get
+                        originalOverloadedProperties[arg] = split
+                        prop.set(split)
+                    }
+
+                    arguments.remove(arg)
+                    return@forEach
+                }
+
+                ////
+                // CLI CHECK IF PROPERTY EXIST  (check if 'arg' exists, and is a boolean)
+                // if arg is found, no more processing happens
+                ////
+                if (returnType.isSubclassOf(Boolean::class)) {
+                    // this is a boolean type? if present then we make it TRUE
+                    foundArg = arguments.firstOrNull { it.startsWith(arg) }
+                    if (foundArg != null) {
+                        // save this, so we can figure out what the values are on save (and not save overloaded properties, that are unchanged)
+                        val get = prop.get()
+                        if (get != true) {
+                            // only track it if it's different
+                            trackedConfigProperties[arg] = get
+                            originalOverloadedProperties[arg] = true
+                            prop.set(true)
+                        }
+
+                        arguments.remove(arg)
+                        return@forEach
+                    }
+                }
+
+
+                ////
+                // SYSTEM PROPERTY CHECK
+                ////
+                var sysProperty: String? = OS.getProperty(arg)?.trim()
+
+                // try lowercase
+                if (sysProperty.isNullOrEmpty()) {
+                    sysProperty = OS.getProperty(arg.lowercase(Locale.getDefault()))?.trim()
+                }
+                // try uppercase
+                if (sysProperty.isNullOrEmpty()) {
+                    sysProperty = OS.getProperty(arg.uppercase(Locale.getDefault()))?.trim()
+                }
+
+                if (!sysProperty.isNullOrEmpty()) {
+                    val get = prop.get()
+                    if (get != sysProperty) {
+                        // only track it if it's different
+                        trackedConfigProperties[arg] = get
+                        originalOverloadedProperties[arg] = sysProperty
+                        prop.set(sysProperty)
+                    }
+
+                    return@forEach
+                }
+
+
+                ////
+                // ENVIRONMENT VARIABLE CHECK
+                ////
+                var envProperty = OS.getEnv(environmentVarPrefix + arg)?.trim()
+
+                // try lowercase
+                if (envProperty.isNullOrEmpty()) {
+                    envProperty = OS.getEnv(environmentVarPrefix + arg.lowercase(Locale.getDefault()))?.trim()
+                }
+                // try uppercase
+                if (envProperty.isNullOrEmpty()) {
+                    envProperty = OS.getEnv(environmentVarPrefix + arg.uppercase(Locale.getDefault()))?.trim()
+                }
+
+                if (!envProperty.isNullOrEmpty()) {
+                    val get = prop.get()
+                    if (get != envProperty) {
+                        // only track it if it's different
+                        trackedConfigProperties[arg] = get
+                        originalOverloadedProperties[arg] = envProperty
+                        prop.set(envProperty)
+                    }
+
+                    return@forEach
+                }
+            } else {
+               logger.error("${prop.member.name} (${returnType.javaObjectType.simpleName}) overloading is not supported. Ignoring")
+            }
+        }
+    }
+}
